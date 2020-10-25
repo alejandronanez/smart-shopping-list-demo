906b054ca31e9edfdd616671a56b4767c7c15b43
Alejandro Nanez <alejonanez@gmail.com>
2020-10-24 22:44:07 -0500

    Configure Firebase.

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,13 @@
-import firebase from '@firebase/app';
-// Initalize Firebase.
-export const config = {
-  apiKey: 'AIzaSyCGGw8RL0XcTZo003RWBDp52yADBDlNwlc',
-  authDomain: 'tcl-14-smart-shopping-list.firebaseapp.com',
-  databaseURL: 'https://tcl-14-smart-shopping-list.firebaseio.com',
-  projectId: 'tcl-14-smart-shopping-list',
-  storageBucket: 'tcl-14-smart-shopping-list.appspot.com',
-  messagingSenderId: '939033171046',
-  appId: '1:939033171046:web:53cbe6fd4e5a4f600b09a7',
-};
+import firebase from 'firebase/app';
 
-const initializedApp = firebase.initializedApp(config);
-
-export default initializedApp;
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCGGw8RL0XcTZo003RWBDp52yADBDlNwlc',
+    authDomain: 'tcl-14-smart-shopping-list.firebaseapp.com',
+    databaseURL: 'https://tcl-14-smart-shopping-list.firebaseio.com',
+    projectId: 'tcl-14-smart-shopping-list',
+    storageBucket: 'tcl-14-smart-shopping-list.appspot.com',
+    messagingSenderId: '939033171046',
+    appId: '1:939033171046:web:53cbe6fd4e5a4f600b09a7',
+  });
+}
